83bd04d9c71ef628139e444cfc648d0703903848
AltaalibIso <129578270+AltaalibIso@users.noreply.github.com>
2024-06-27 11:57:45 +0300

    Update sqliteMode.py

--- sqliteMode.py
+++ sqliteMode.py
@@ -1,13 +1,62 @@
-#import modules 
+#import modules
+import os
 import sqlite3 as sq
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-#Connecting to a database and creating a cursor
-con = sq.connect("bd.db", check_same_thread=False)
+db_path = 'bd.db'
+
+
+# Check if database exists, create if it doesn't
+if not os.path.exists(db_path):
+    open(db_path, 'w').close()
+
+
+# Connecting to a database and creating a cursor
+con = sq.connect(db_path, check_same_thread=False)
 cur = con.cursor()
 
 
+def TableExists(table_name):
+    """Check if a table exists in the database"""
+    print('start TableExists')
+    cur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name=?", (table_name,))
+    if cur.fetchone()[0] == 1:
+        return True
+    else:
+        return False
+
+
+def CreateTable(table_name):
+    """Create a table in the database"""
+    print("start CreateTable")
+    columns = ""
+    match table_name:
+        case 'users':
+            columns = 'id TEXT PRIMARY KEY, name TEXT, numb TEXT, id_tg TEXT, surname TEXT'
+        case 'trips':
+            # columns = 'user_id TEXT, typeofmembers TEXT, tripsdates TEXT, tripstimes TEXT, direction_name TEXT, route_number TEXT, pointa TEXT, pointb TEXT, id_trip TEXT, number_of_passengers TEXT, status TEXT'
+            # new version of table VVVVVVVV
+            columns = 'user_id TEXT, typeofmembers TEXT, tripsdates TEXT, tripstimes TEXT, direction_name TEXT, route_number INTEGER, pointa INTEGER, pointb INTEGER, id_trip TEXT, number_of_passengers INTEGER, status TEXT'
+        case 'transactions':
+            columns = 'id TEXT PRIMARY KEY, user_id, summ TEXT, date_time TEXT, type_of_transaction TEXT'
+        case 'drivers':
+            columns = 'user_id TEXT, brand TEXT, colour TEXT, numbcar TEXT, car_id TEXT'
+        case 'balance':
+            columns = 'id TEXT PRIMARY KEY, user_id TEXT, summ TEXT'
+        case 'agreedTrips':
+            # columns = 'id_trip TEXT, tripsdates TEXT, tripstimes TEXT, pointa TEXT, pointb TEXT, number_of_passengers TEXT, id_driver TEXT, id_passenger TEXT, status TEXT, ids_trips TEXT, maximum_number_of_passengers TEXT'
+            # new version of table VVVVVVVV
+            columns = 'agreeding_trips_id TEXT, driver_trip_id TEXT, maximum_number_of_passengers INTEGER, number_of_passengers INTEGER, ids_trips TEXT, status TEXT'
+        case 'agreement':
+            columns = 'id_agreement TEXT, id_user TEXT, response INT, datetime TEXT'
+        case _:
+            print("start CreateTable Unknown table name")
+            raise ValueError(f"Unknown table name '{table_name}'")
+    print("columns: ", columns)
+    print(f"CREATE TABLE {table_name}({columns})")
+
+    cur.execute(f"CREATE TABLE {table_name}({columns})")
 
 
 def CheckUserIdTg(id_tg):
@@ -19,18 +68,17 @@ def CheckUserIdTg(id_tg):
         return []
 
 
-
-
 def InsertData(T, V, C=""):
     """Entering data into the database"""
     try:
+        if not TableExists(T):
+            CreateTable(T)
         cur.execute(f'INSERT INTO {T} {C} VALUES({V})')
         con.commit()
-        return[1, 2]
+        return [1, 2]
     except Exception as e:
+        print(e)
         return []
-
-
 
 
 def DeleteData(T, V, C):
@@ -80,9 +128,3 @@ def SelectAllData(T, C, V, S="*"):
         return newList
     except Exception as e:
         return []
-
-
-
-
-
-
